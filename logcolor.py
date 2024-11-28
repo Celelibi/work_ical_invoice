303be9a89c4865e420942906bf376827ade20c27
Celelibi <celelibi@gmail.com>
2024-11-28 15:59:16 +0100

    logcolor.py: add docstrings
    Signed-off-by: Celelibi <celelibi@gmail.com>

--- logcolor.py
+++ logcolor.py
@@ -1,9 +1,17 @@
+"""Custom log formatters.
+
+This module defines custom log formatters class. Its content is designed to be
+referenced by logconf.ini alone.
+"""
+
 import logging
 import colorama
 
 
 
 class ColorLogFormatter(logging.Formatter):
+    """Log formatter that adds color to the debug level word."""
+
     namecolors = {
         'DEBUG': colorama.Fore.BLUE,
         'INFO': colorama.Fore.GREEN,
@@ -21,5 +29,13 @@ class ColorLogFormatter(logging.Formatter):
         return colorama.Style.BRIGHT + s + name + colorama.Style.RESET_ALL
 
     def format(self, record):
+        """Set records's attribute levelnamecolor and call superclass's format
+        method.
+
+        Attribute record.levelnamecolor hold the name of the log level plus any
+        terminal code to make it colored. It can be used in the format given in
+        logconf.ini.
+        """
+
         record.levelnamecolor = self._colorname(record.levelname)
         return super().format(record)
